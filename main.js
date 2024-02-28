82fe6d978633d87a67a8342bc5c7b232fd822ec2
usaid2007 <161623251+usaid2007@users.noreply.github.com>
2024-02-28 22:26:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Question: 1
+// console.log("Hello World");
+// Question: 2
+// var myname = 'Usaid';
+// var message = 'like to learn some Python today';
+// console.log(myname + " " + message);
---
+++ main.ts
@@ -0,0 +1,10 @@
+// Question: 1
+
+// console.log("Hello World");
+
+
+ // Question: 2
+
+ // let myname = 'Usaid';
+ // let message = 'like to learn some Python today';
+ // console.log(myname+" "+message);
